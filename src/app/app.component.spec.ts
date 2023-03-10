c82c78d738a2c6ffa96f62c690973ffacb104684
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-03-10 17:09:02 -0500

    Add Components Concat, Copywithin, and Navbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,12 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from '../app/components/welcome/welcome.component'
 import { NavbarComponent } from "./components/navbar/navbar.component"
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [
         AppComponent,
         WelcomeComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ConcatComponent } from './components/array/concat/concat.component';
 import { CopywithinComponent } from './components/array/copywithin/copywithin.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+let appRoutes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'array/concat', component: ConcatComponent },
+  { path: 'array/copywithin', component: CopywithinComponent  },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +25,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
