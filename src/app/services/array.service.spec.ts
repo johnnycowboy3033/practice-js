1ac52bf9a4c9fc45757763880d49bd08d9fb007d
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-03-11 14:56:03 -0500

    Writing unit for the assignment of the CopyWithin context

--- src/app/services/array.service.spec.ts
+++ src/app/services/array.service.spec.ts
@@ -10,7 +10,11 @@ describe('ArrayService', () => {
     service = TestBed.inject(ArrayService);
   });
 
-  it('should be created', () => {
+  /***********************\
+   *  Service             *
+  \***********************/
+
+  it('(Service) should be created', () => {
     expect(service).toBeTruthy();
   });
 
@@ -18,23 +22,23 @@ describe('ArrayService', () => {
    * TABLES               *
   \***********************/
 
-  it('should be the Order Integer array should be twelve in length', () => {
+  it('(TABLES) should be the Order Integer array should be twelve in length', () => {
     expect(service.OrderInteger.length).toEqual(12);
   });
 
-  it('should be the Fruit array should be seven in length', () => {
+  it('(TABLES) should be the Fruit array should be seven in length', () => {
     expect(service.Fruits.length).toEqual(7);
   });
 
-  it('should be the Unique Integer array should be fifteen in length', () => {
+  it('(TABLES) should be the Unique Integer array should be fifteen in length', () => {
     expect(service.UniqueInteger.length).toEqual(15);
   });
 
-  it('should be the Peoples array should be five in length', () => {
+  it('(TABLES) should be the Peoples array should be five in length', () => {
     expect(service.Peoples.length).toEqual(5);
   });
 
-  it('should be the Tables Map will be of size four', () => {
+  it('(TABLES) should be the Tables Map will be of size four', () => {
     expect(service.tables.size).toEqual(4);
   });
 
@@ -42,11 +46,11 @@ describe('ArrayService', () => {
    * Enumerator           *
   \***********************/
 
-  it('should be four the size of the Array Names Enumerator', () => {
+  it('(Enumerator) should be four the size of the Array Names Enumerator', () => {
     expect(service.enumElementCount(service.getArrayNames())).toEqual(4);
   });
 
-  it('should be thirty-one the size of the Component Name Enumerator', () => {
+  it('(Enumerator) should be thirty-one the size of the Component Name Enumerator', () => {
     expect(service.enumElementCount(service.getComponentName())).toEqual(31);
   });
 
@@ -54,7 +58,7 @@ describe('ArrayService', () => {
    * Concatenate Context  *
   \***********************/
 
-  it('should be two for the total number an arrays for Initialise Array ', () => {
+  it('(Concatenate) should be two for the total number an arrays for Initialise Array ', () => {
 
     let concatContext =  service.context.get( service.getComponentName().Concatenate);
     expect(concatContext.Begin.Tables.length).toEqual(2);
@@ -62,7 +66,7 @@ describe('ArrayService', () => {
 
   });
 
-  it('should be Fruits for the table name for the Initialise Array ', () => {
+  it('(Concatenate) should be Fruits for the table name for the Initialise Array ', () => {
 
     let concatContext =  service.context.get( service.getComponentName().Concatenate);
     expect(concatContext.Begin.DefaultNames[0]).toEqual(service.getArrayNames().Fruits);
@@ -70,7 +74,7 @@ describe('ArrayService', () => {
 
   });
 
-  it('should be "Banana" for the first element in the Fruits Array for Initialise Array', () => {
+  it('(Concatenate) should be "Banana" for the first element in the Fruits Array for Initialise Array', () => {
 
     let concatContext =  service.context.get( service.getComponentName().Concatenate);
     let fruits = concatContext.Begin.Tables[0];
@@ -79,7 +83,7 @@ describe('ArrayService', () => {
 
   });
 
-  it('should be "Cecilie" for the first element in the Peoples Array for Initialise Array', () => {
+  it('(Concatenate) should be "Cecilie" for the first element in the Peoples Array for Initialise Array', () => {
 
     let concatContext =  service.context.get( service.getComponentName().Concatenate);
     let fruits = concatContext.Begin.Tables[1];
@@ -88,12 +92,30 @@ describe('ArrayService', () => {
 
   });
 
-  it('should be true for the Should Compare Variable', () => {
+  it('(Concatenate) should be true for the Should Compare Variable', () => {
 
     let concatContext =  service.context.get( service.getComponentName().Concatenate);
     expect(concatContext.ShouldCompare).toEqual(true);
 
   });
 
+  /***********************\
+   * CopyWithin  Context  *
+  \***********************/
+
+  it('(CopyWithin) should be two for the total number an arrays for Initialise Array ', () => {
+
+    let concatContext =  service.context.get( service.getComponentName().CopyWithin);
+    expect(concatContext.Begin.Tables.length).toEqual(1);
+    expect(concatContext.Begin.DefaultNames.length).toEqual(1);
+
+  });
+
+  it('(CopyWithin) should be true for the Should Compare Variable', () => {
+
+    let concatContext =  service.context.get( service.getComponentName().CopyWithin);
+    expect(concatContext.ShouldCompare).toEqual(false);
+
+  });
 
 });
